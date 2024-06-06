6dca9099024ab6d235736e38db29f4848689d993
jussara2 <144683878+jussara2@users.noreply.github.com>
2024-06-06 14:02:09 -0300

    provainfor

--- cadastro.php
+++ cadastro.php
@@ -9,34 +9,22 @@
 </head>
 
 <body>
-    <?php 
-    include("conexao.php");
-    $id = $_GET["id"] ?? '';
-    $sql = "SELECT * FROM usuario WHERE id = $id";
-
-    $dados = mysqli_query($conexao,$sql);
-    $linha = mysqli_fetch_assoc($dados);
-    
-    
-    ?>
   <div class="container">
     <div class="row">
       <div class="col">
-        <h1>Alteração de dados</h1>
-        <form action="editcad.php" method="POST">
+        <h1>Cadastre-se</h1>
+        <form action="cad.php" method="POST">
           <div class="mb-3">
             <label for="nome" class="form-label">Nome Completo</label>
-            <input type="text" class="form-control" name="nome"required value="<?php echo $linha['nome'] ;?>">
+            <input type="text" class="form-control" name="nome">
 
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" name="email" value="<?php echo $linha['email'] ;?>">
+            <input type="email" class="form-control" name="email">
 
             <label for="senha" class="form-label">Senha</label>
-            <input type="password" class="form-control" name="senha"value="<?php echo $linha['senha'] ;?>">
+            <input type="password" class="form-control" name="senha">
 
-            <input type="submit" class="btn btn-success" value="salvar alterações">
-            <input type="hidden" name="id" value="<?php echo $linha['id'] ;?>">
-            
+            <input type="submit" class="btn btn-success">
 
           </div>
       </div>
--- editecad.php
+++
@@ -1,9 +0,0 @@
-<?php
-include ("conexao.php");
-$id=$_POST['id'];
-$nome=$_POST['nome'];
-$email=$_POST['email'];
-$senha= password_hash($_POST['senha'],PASSWORD_DEFAULT);
- 
-
-$sql =
